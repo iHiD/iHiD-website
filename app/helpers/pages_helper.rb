159891124e7cf39e8bc7690f38522ed98606ad74
Jeremy Walker <jeremy@meducation.net>
2012-04-18 18:14:24 +0200

    Fixed broken links in sitemap

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,9 +1,9 @@
 module PagesHelper
   
   def sitemap_url_tag(url, lastmod, changefreq, priority)
-    loc = "https://www.ihid.co.uk#{url}" if url.starts_with?("/")
+    url = "http://www.ihid.co.uk#{url}" if url.starts_with?("/")
     content_tag(:url) do
-      content_tag(:loc, loc) + 
+      content_tag(:loc, url) + 
       content_tag(:lastmod, lastmod.strftime("%Y-%m-%dT%H:%MZ")) + 
       content_tag(:changefreq, changefreq) + 
       content_tag(:priority,  priority)
--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ApplicationHelper do
   describe "sitemap_url_tag" do
     it "should return the correct xml" do
-      sitemap_xml = "<url><loc>https://www.ihid.co.uk/pages/1</loc><lastmod>2012-02-17T00:00Z</lastmod><changefreq>weekly</changefreq><priority>1</priority></url>"
+      sitemap_xml = "<url><loc>http://www.ihid.co.uk/pages/1</loc><lastmod>2012-02-17T00:00Z</lastmod><changefreq>weekly</changefreq><priority>1</priority></url>"
       helper.sitemap_url_tag("/pages/1", DateTime.new(2012,2,17), "weekly", 1).should == sitemap_xml
     end
   end

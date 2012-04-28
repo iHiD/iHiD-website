5afc85f28f076a978aa4b44d871e443ce4c17bf4
Jeremy Walker <jeremy@meducation.net>
2012-04-28 22:26:42 +0100

    Redirect to www from naked domain.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
 
-  protect_from_forgery  
+  protect_from_forgery
+  before_filter :check_uri
   before_filter :set_metadata
   before_filter :get_projects
   
   private
+
+  def check_uri
+    redirect_to "#{request.protocol}www.#{request.host_with_port}#{request.fullpath}" if !/^www/.match(request.host) if Rails.env.production?
+    true
+  end
   
   def set_metadata
     @metadata ||= {}

1b3852639157608a98169d33dfd86458f0dec446
Jeremy Walker <jeremy@meducation.net>
2012-05-20 13:57:31 +0100

    Add some routes that were 404ing.

--- config/routes.rb
+++ config/routes.rb
@@ -23,17 +23,23 @@ Blog::Application.routes.draw do
   ###################
   get "author/ihid" => redirect("/blog")
 
-  get "category/webrat" => redirect("/blog")
-  get "category/uncategorized" => redirect("/blog")
-  get "category/gems/haml" => redirect("/blog")
-  get "category/ruby-on-rails" => redirect("/blog")
-  get "category/ruby-on-rails/ruby-on-rails-3-2/feed" => redirect("/blog")
+  get "category/*subcategory" => redirect("/blog")
 
   get "page/2" => redirect("/blog")
 
+  get "2011/03" => redirect("/blog")
   get "2011/05" => redirect("/blog")
+  get "2011/06" => redirect("/blog")
   get "2012/01" => redirect("/blog")
   get "2012/02" => redirect("/blog")
+  
+  get "about_me" => redirect("/pages/about")
+  
+  get "blog/creating-an-ec2-webserver-using-amazon-s-basic-ami--2" => redirect("blog/creating-an-ec2-webserver-using-amazon-s-basic-ami")
+  get "removing-duplicate-code-by-using-blocks-in-ruby" => redirect("/blog")
+  get "having-a-hobby-project" => redirect("/blog")
+  get "blog/schema.org" => redirect("/blog/markup-your-blog-using-schema-org")
+  get "blog/search" => redirect("/blog")
 
   {
     "a-scope-for-returning-no-records-in-activerecord" => "a-scope-for-returning-no-records-in-activerecord",
@@ -47,9 +53,12 @@ Blog::Application.routes.draw do
     "iphone-error-the-user-name-or-password-for-imap-gmail-com-is-incorrect" => "iphone-error-the-user-name-or-password-for-imap-gmail-com-is-incorrect",
     "why-ed-miliband-not-ken-clarke-should-be-apologising-tonight" => "why-ed-miliband-not-ken-clarke-should-be-apologising-tonight",
     "fixed-foreigner-gem-to-work-with-reversing-migrations-in-rails-3-1" => "fixed-foreigner-gem-to-work-with-reversing-migrations-in-rails-3-1",
-    "belongs_to_enum-is-released" => "belongs_to_enum-is-released"
-    
-  }.each do |old_url, blog_post_title|
+    "belongs_to_enum-is-released" => "belongs_to_enum-is-released",
+    "testing-views-with-rails-3-rspec-2-and-webrat" => 'testing-views-with-rails-3-rspec-2-and-webrat',
+    "haml-is-broken-with-partial-layouts" => "haml-is-broken-with-partial-layouts",
+    "creating-a-ec2-webserver-using-amazons-basic-ami" => "creating-an-ec2-webserver-using-amazon-s-basic-ami",
+    "form-select-tag-does-not-accept-ranges-in-rails-3-2" => "form-select-tag-does-not-accept-ranges-in-rails-3-2",
+    }.each do |old_url, blog_post_title|
     get old_url => redirect("/blog/#{blog_post_title}")
     get "#{old_url}/feed" => redirect("/blog/#{blog_post_title}")
   end
